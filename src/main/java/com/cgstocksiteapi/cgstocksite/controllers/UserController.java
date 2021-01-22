18d8d8f0a296100ede76bffa348c38a6eee400e1
Christian Gracia <christiangracia.student@careerdevs.com>
2021-01-22 00:31:37 -0500

    removed spring security and made / route give 200 http status for kubernetes deploy

--- src/main/java/com/cgstocksiteapi/cgstocksite/controllers/UserController.java
+++ src/main/java/com/cgstocksiteapi/cgstocksite/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping(path="/users")
+    @GetMapping(path="/")
     public String sendMessage() {
 //        URL url = null;
 //        try {
